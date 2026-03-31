70141a99ab043729bc04169e8d89853ef5924062
cxthya <calderonhernandezcinthya@gmail.com>
2026-03-31 13:14:18 -0400

    dictionary update

---
+++ dictionary_update_remove.py
@@ -0,0 +1,16 @@
+'''
+Cinthya Calderon-Hernandez
+CSMC 111
+Spring 2026
+Assignment 3: Updating and Removing Dictionary Elements
+'''
+
+#Provided dictionary
+car = {
+    "brand": "Toyota",
+    "model": "Camry",
+    "year": 2018
+}
+#update the year
+car["year"] = 2022
+print("Updated Car Dictionary:", car)
