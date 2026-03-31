6005997c7a18453097e055b31fcc413d9d11f146
cxthya <calderonhernandezcinthya@gmail.com>
2026-03-31 13:06:45 -0400

    Create dictionary_retrieval.py

---
+++ dictionary_retrieval.py
@@ -0,0 +1,12 @@
+'''
+Cinthya Calderon-Hernandez
+CSMC 111
+Spring 2026
+Assignment 2: Dictionary Retrieval
+'''
+#Make the dictionary
+employee = {"name": "Alice", "department": "IT", "salary": 50000}
+department = employee["department"]
+salary = employee["salary"]
+print("Employee Department:", department)
+print("Employee Salary:", salary)
