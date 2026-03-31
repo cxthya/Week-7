95fa8364549ae815ed999c34ab526b4e602a1931
cxthya <calderonhernandezcinthya@gmail.com>
2026-03-31 13:01:31 -0400

    student dictionary

---
+++ student_dictionary.py
@@ -0,0 +1,13 @@
+'''
+Cinthya Calderon-Hernandez
+CSMC 111
+Spring 2026
+Assignment 1: Creating and Printing Dictionaries
+'''
+#Creat the dictionary
+student = {
+    "name": "Poopy McPooperson",
+    "age": 20,
+    "major": "Health Services"
+}
+print("Student Dictionary:", student)
